59949630f540538621bc4e9aebdbba379c082e48
Bruce Degar <bruce.degar@gerdau.com>
2023-03-02 22:47:02 +1100

    ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// address of NFT contract address that you has deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0E926ee71b82b9AdFA0EDfC31B999081408F2ab0";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
